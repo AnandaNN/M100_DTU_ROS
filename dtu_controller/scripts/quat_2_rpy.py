34b8b78fc84a80e2bbec0a4e20f9ffc0c9e73452
Ananda <ananda_nielsen@yahoo.dk>
2020-10-20 16:39:33 +0200

    Added quat 2 rpy script

---
+++ dtu_controller/scripts/quat_2_rpy.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import rospy
+import tf
+
+# ROS messages.
+from geometry_msgs.msg import QuaternionStamped
+from geometry_msgs.msg import TransformStamped
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Joy
+from guidance.msg import Motion
+
+import numpy as np
+
+RAD2DEG = 180.0*np.pi 
+
+class QuatToEuler():
+    def __init__(self):
+
+        # Create subscribers and publishers.
+        self.djiAttitudeSub   = rospy.Subscriber("/dji_sdk/attitude", QuaternionStamped, self.attitudeCallback)
+        self.guidanceImuSub  = rospy.Subscriber("/guidance/imu", TransformStamped, self.guidanceImuCallback)
+        self.cmdSub = rospy.Subscriber("/dji_sdk/flight_control_setpoint_rollpitch_yawrate_zvelocity/", Joy, self.cmdCallback)
+        self.motionSub = rospy.Subscriber("/guidance/motion", Motion, self.motionCallback)
+        
+        self.attitudePub = rospy.Publisher("attitudeRPY", Vector3, queue_size=1)
+        self.guidanceRPYPub = rospy.Publisher("guidanceRPY", Vector3, queue_size=1)
+        self.cmdPub = rospy.Publisher("cmdRP", Vector3, queue_size=1)
+        self.motionPub = rospy.Publisher("motionRPY", Vector3, queue_size=1)
+
+        # Main while loop.
+        rospy.spin()
+
+    # Odometry callback function.
+    def attitudeCallback(self, msg):
+        # Convert quaternions to Euler angles.
+        (r, p, y) = tf.transformations.euler_from_quaternion([msg.quaternion.x, msg.quaternion.y, msg.quaternion.z, msg.quaternion.w])
+        outMsg = Vector3()
+        outMsg.x = r
+        outMsg.y = p
+        outMsg.z = -y
+        self.attitudePub.publish(outMsg)
+
+    # IMU callback function.
+    def guidanceImuCallback(self, msg):
+        # Convert quaternions to Euler angles.
+        (r, p, y) = tf.transformations.euler_from_quaternion([msg.transform.rotation.x, msg.transform.rotation.y, msg.transform.rotation.z, msg.transform.rotation.w])
+        outMsg = Vector3()
+        outMsg.x = r
+        outMsg.y = -p
+        outMsg.z = y-np.pi/2.0
+        self.guidanceRPYPub.publish(outMsg)
+
+    def cmdCallback(self, msg):
+        outMsg = Vector3()
+        outMsg.x = msg.axes[0]
+        outMsg.y = msg.axes[1]
+        outMsg.z = msg.axes[3]
+        self.cmdPub.publish(outMsg)
+
+    def motionCallback(self, msg):
+        outMsg = Vector3()
+        (r, p, y) = tf.transformations.euler_from_quaternion([msg.q2, msg.q1, msg.q0, msg.q3])
+        outMsg = Vector3()
+        outMsg.x = r
+        outMsg.y = p
+        outMsg.z = y
+        self.motionPub.publish(outMsg)
+
+# Main function.    
+if __name__ == '__main__':
+    # Initialize the node and name it.
+    rospy.init_node('quat_to_euler')
+    # Go to class functions that do all the heavy lifting. Do error checking.
+    try:
+        quat_to_euler = QuatToEuler()
+    except rospy.ROSInterruptException: pass
