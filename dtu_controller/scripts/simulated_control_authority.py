9be58e9b9b1c3ca3780d770798f1b938addcadbc
DavidWuthier <dwuthier@gmail.com>
2020-11-11 13:17:33 +0100

    Make the control authority server actually deal with control authority and gamepad control

--- dtu_controller/scripts/simulated_control_authority.py
+++ dtu_controller/scripts/simulated_control_authority.py
@@ -1,16 +1,36 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from rospy import init_node, spin, Service
-from dji_sdk.srv import SDKControlAuthority, SDKControlAuthorityResponse
+from rospy import init_node, spin, Service, Subscriber, Publisher
+from rospy import loginfo
+from dji_sdk.srv import SDKControlAuthority, SDKControlAuthorityResponse, SDKControlAuthorityRequest
+from sensor_msgs.msg import Joy
 
 class ControlAuthorityServer():
   def __init__(self):
-    sca = Service('/dji_sdk/sdk_control_authority', SDKControlAuthority, self.handler)
+    self.authority = False
+
+    control_authority_srv = Service('/dji_sdk/sdk_control_authority', SDKControlAuthority, self.handler)
+
+    self.control_pub = Publisher('/dji_sdk/matlab_rpvy', Joy, queue_size = 1)
+
+    joy_sub = Subscriber('/dji_sdk/xbox_joy', Joy, self.joy_callback)
+    control_sub = Subscriber('/dji_sdk/flight_control_setpoint_rollpitch_yawrate_zvelocity', Joy, self.control_callback)
 
   def handler(self, request):
-    res = SDKControlAuthorityResponse(True, 0, 0, 0)
-    return res
+    self.authority = request.control_enable
+    loginfo('authority = %s', self.authority)
+    return SDKControlAuthorityResponse(True, 0, 0, 0)
+
+  def joy_callback(self, msg):
+    self.authority = False
+    a = msg.axes
+    joy = Joy(axes = [-a[2], a[3], 2*a[1], 2*a[0]])
+    self.control_pub.publish(joy)
+
+  def control_callback(self, msg):
+    if self.authority:
+      self.control_pub.publish(msg)
 
 if __name__ == '__main__':
   init_node('simulated_control_authority_server')
